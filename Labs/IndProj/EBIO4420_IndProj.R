ecc6c36db892f20743d2225a0fbe32087c582277
kenyagates <gateskenya0@gmail.com>
2020-04-25 14:24:41 -0600

    Includes for loops for all community classes

--- Labs/IndProj/EBIO4420_IndProj.R
+++ Labs/IndProj/EBIO4420_IndProj.R
@@ -55,11 +55,9 @@ typeof(SpeciesN)
 "Total_Species" <- as.character(unique(data$Species, incomparables = F))
 Total_Species <- na.omit(Total_Species)
 
-# Creating a for loop to compare species lists between comunity classes 
 
 
-
-# Trying to consolidate Barren for loops into one 
+## For loop comparing barren plots species lists with all other community class species lists  
 
 "Matches_BP/SB" = c() # trying to create a vector container
 "Matches_BP/WM" = c()
@@ -104,3 +102,119 @@ for (i in 1:length(BP_Species)) {
 `Matches_BP/DM`
 `Matches_BP/FF`
 
+
+## Now for Snow Bed plots vs. remaining
+
+# creating vector containers
+
+"Matches_SB/WM" = c()
+"Matches_SB/MM" = c()
+"Matches_SB/DM" = c()
+"Matches_SB/FF" = c()
+
+
+
+for (i in 1:length(SB_Species)) {
+  for (j in 1:length(WM_Species)){
+    if (SB_Species[i] == WM_Species[j]) { 
+      `Matches_SB/WM`[length(`Matches_SB/WM`)+1] = c(SB_Species[i], i)
+    }
+  } 
+  for (j in 1:length(MM_Species)){
+    if (SB_Species[i] == MM_Species[j]) { 
+      `Matches_SB/MM`[length(`Matches_SB/MM`)+1] = c(SB_Species[i], i)
+    }
+  }
+  for (j in 1:length(DM_Species)){
+    if (SB_Species[i] == SB_Species[j]) { 
+      `Matches_SB/DM`[length(`Matches_SB/DM`)+1] = c(SB_Species[i], i)
+    }
+  }
+  for (j in 1:length(FF_Species)){
+    if (SB_Species[i] == FF_Species[j]) { 
+      `Matches_SB/FF`[length(`Matches_SB/FF`)+1] = c(SB_Species[i], i)
+    }
+  }
+  
+}
+
+`Matches_SB/WM`
+`Matches_SB/MM`
+`Matches_SB/DM`
+`Matches_SB/MM`
+
+
+## Wet Meadow vs. remaining
+
+# creating vector containers
+
+"Matches_WM/MM" = c()
+"Matches_WM/DM" = c()
+"Matches_WM/FF" = c()
+
+
+
+for (i in 1:length(WM_Species)) {
+  for (j in 1:length(MM_Species)){
+    if (WM_Species[i] == MM_Species[j]) { 
+      `Matches_WM/MM`[length(`Matches_WM/MM`)+1] = c(WM_Species[i], i)
+    }
+  }
+  for (j in 1:length(DM_Species)){
+    if (WM_Species[i] == DM_Species[j]) { 
+      `Matches_WM/DM`[length(`Matches_WM/DM`)+1] = c(WM_Species[i], i)
+    }
+  }
+  for (j in 1:length(FF_Species)){
+    if (WM_Species[i] == FF_Species[j]) { 
+      `Matches_WM/FF`[length(`Matches_WM/FF`)+1] = c(WM_Species[i], i)
+    }
+  }
+  
+}
+
+`Matches_WM/MM`
+`Matches_WM/DM`
+`Matches_WM/MM`
+ 
+
+## Moist Meadow vs remaining 
+
+"Matches_MM/DM" = c()
+"Matches_MM/FF" = c()
+
+
+
+for (i in 1:length(MM_Species)) {
+  for (j in 1:length(DM_Species)){
+    if (MM_Species[i] == DM_Species[j]) { 
+      `Matches_MM/DM`[length(`Matches_MM/DM`)+1] = c(MM_Species[i], i)
+    }
+  }
+  for (j in 1:length(FF_Species)){
+    if (MM_Species[i] == FF_Species[j]) { 
+      `Matches_MM/FF`[length(`Matches_MM/FF`)+1] = c(MM_Species[i], i)
+    }
+  }
+  
+}
+
+`Matches_MM/DM`
+`Matches_MM/FF`
+
+## Dry meadow against fellfield 
+
+"Matches_DM/FF" = c()
+
+
+
+for (i in 1:length(DM_Species)) {
+  for (j in 1:length(FF_Species)){
+    if (DM_Species[i] == FF_Species[j]) { 
+      `Matches_DM/FF`[length(`Matches_DM/FF`)+1] = c(DM_Species[i], i)
+    }
+  }
+  
+}
+
+`Matches_DM/FF`
